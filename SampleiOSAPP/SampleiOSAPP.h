a31fa55b830f4acc985a1b24b804500dbe01b09d
vijay <vdmasal@gmail.com>
2018-06-22 17:29:28 +0530

    Initial Commit

---
+++ SampleiOSAPP/SampleiOSAPP.h
@@ -0,0 +1,19 @@
+//
+//  SampleiOSAPP.h
+//  SampleiOSAPP
+//
+//  Created by Emberify_Vijay on 22/06/18.
+//  Copyright © 2018 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleiOSAPP.
+FOUNDATION_EXPORT double SampleiOSAPPVersionNumber;
+
+//! Project version string for SampleiOSAPP.
+FOUNDATION_EXPORT const unsigned char SampleiOSAPPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleiOSAPP/PublicHeader.h>
+
+
